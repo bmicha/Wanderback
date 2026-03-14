3b3c1ec204de4b93e6cd1b8cb31181692e826a79
Bastien <bmicha@me.com>
2026-03-14 18:34:46 +0100

    Initial Commit

---
+++ Wanderback/WanderbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderbackApp.swift
+//  Wanderback
+//
+//  Created by Bastien Micha on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WanderbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
